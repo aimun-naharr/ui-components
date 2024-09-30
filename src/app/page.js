aa2b7c55a002702415f2f7fcaefaedfa5e43001a
aimunNaharr <aimunnaharr@gmail.com>
2024-09-30 22:07:04 +0600

    update carousel

--- src/app/page.js
+++ src/app/page.js
@@ -9,11 +9,11 @@ export default function Home() {
   return (
     <div className=" my-10">
       <Container>
-        <h1>Button variants</h1>
+        <h1 className="mb-4 font-semibold">Button variants</h1>
         <Link href={'/buttons'} className="bg-zinc-200 px-6 py-2">Explore Buttons</Link>
 
         <div className="my-10">
-          <MobbinCarousel />
+          {/* <MobbinCarousel /> */}
         </div>
       </Container>
     </div>
--- src/components/MobbinCarousel.js
+++ src/components/MobbinCarousel.js
@@ -1,7 +1,52 @@
-import React from 'react'
+'use client'
+
+import Link from "next/link";
+import { useState } from "react";
+import { flushSync } from "react-dom";
 
 export default function MobbinCarousel() {
+  const [activeItem, _setActiveItem] = useState( null )
+
+  const setActiveItem = ( id ) => {
+    if ( document.startViewTransition ) {
+      document.startViewTransition( () => {
+        flushSync( () => {
+          _setActiveItem( id )
+        } )
+      } )
+    } else {
+      _setActiveItem( id )
+    }
+
+  }
+  const categories = [
+    { id: 1, name: "Fashion & Apparel", image: "fashion-apparel.jpg" },
+    { id: 2, name: "Beauty & Skincare", image: "beauty-skincare.jpg" },
+    { id: 3, name: "Electronics & Gadgets", image: "electronics-gadgets.jpg" },
+    { id: 4, name: "Home & Kitchen", image: "home-kitchen.jpg" },
+    { id: 5, name: "Health & Wellness", image: "health-wellness.jpg" },
+    { id: 6, name: "Toys & Games", image: "toys-games.jpg" },
+    { id: 7, name: "Sports & Outdoors", image: "sports-outdoors.jpg" },
+    { id: 8, name: "Books & Media", image: "books-media.jpg" },
+    { id: 9, name: "Grocery & Food", image: "grocery-food.jpg" },
+    { id: 10, name: "Pet Supplies", image: "pet-supplies.jpg" }
+  ];
+
   return (
-    <div>MobbinCarousel</div>
+    <div>
+      <div className='flex gap-4 relative'>
+        {
+          categories.map( itm => {
+            return <div onPointerEnter={() => setActiveItem( itm.id )} className='border whitespace-nowrap relative group' key={itm.id}>
+              <Link
+                aria-expanded={activeItem === itm.id} className="peer" href='#'>
+                {itm.name}
+              </Link>
+              <div className="absolute hidden pointer-events-none top-full h-40 border  [view-transition-name: floating-card] peer-aria-expanded:block">modal</div></div>
+          } )
+        }
+
+      </div>
+    </div>
   )
 }
