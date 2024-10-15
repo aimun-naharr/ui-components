96784dbd81a215146611a36876ca597a7923501d
aimun nahar <aimunnaharr@gmail.com>
2024-10-15 17:13:48 +0600

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -10,12 +10,31 @@ export default function Home() {
     <div className=" my-10">
       <Container>
         <h1 className="mb-4 font-semibold">Button variants</h1>
-        <Link href={'/buttons'} className="bg-black text-white rounded shadow-md hover:shadow-yellow-200 transition-shadow px-6 py-3 shadow-yellow-100">Explore Buttons</Link>
+        <Link href={'/buttons'} className=" relative  rounded   z-[2] text-black text-xl  w-full h-full">
 
-        <div className="my-10">
+          <span className="absolute text-xl top-[20px] left-[34px] whitespace-nowrap">Explore Buttons</span>
+
+          {/* btn-inner */}
+          <svg
+            width="10"
+            height="10"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg" className="button-svg absolute top-0 left-0 -z-[1]" overflow="visible"
+            preserveAspectRatio="none"
+
+            style={{ width: '140px', height: '60px' }}>
+            <path d="M36,0 L170,0 C217.34,0 217.34,72 170,72 L36,72 C-11.34,72 -11.34,0 36,0" className="btn-plain__path js-background-path"></path>
+            <path d="M36,0 L170,0 C217.34,0 217.34,72 170,72 L36,72 C-11.34,72 -11.34,0 36,0" className="btn-plain__path js-background-path">
+
+            </path>
+          </svg>
+
+        </Link>
+
+        {/* <div className="my-10">
           <h1 className="mb-4 font-semibold">Mobbin carousel</h1>
           <MobbinCarousel />
-        </div>
+        </div> */}
       </Container>
     </div>
   );
